7e5c3c0d873cc824d8f7661bc45754ca17ea22c4
brandenlopez-909 <blopez4148@gmail.com>
2021-11-27 19:17:31 -0800

    Updated DataBase Creation
    Fixed an issue where adopted pets were not assigned to a person.

--- database creation.sql
+++ database creation.sql
@@ -1,4 +1,5 @@
-# Create contact info table and insert data
+# Create Schema 
+create schema helpingpaw;
 
 #Create and insert into admin table
 CREATE TABLE helpingpaw.admin(username VARCHAR(45) PRIMARY KEY, adminid INT);
@@ -67,7 +68,7 @@ beakSize integer
 );
 INSERT INTO helpingpaw.Bird values( 1 , 5);
 INSERT INTO helpingpaw.Bird values( 7 , 1);
-INSERT INTO helpingpaw.Birdl values( 9 , 3);
+INSERT INTO helpingpaw.Bird values( 9 , 3);
 INSERT INTO helpingpaw.Bird values( 11 ,  4);
 INSERT INTO helpingpaw.Bird values( 15 ,  4);
 INSERT INTO helpingpaw.Bird values( 38 ,  10);
@@ -122,8 +123,19 @@ INSERT INTO helpingpaw.contactinformation VALUES ("s.coleman@gmail.com","3219 Ei
 INSERT INTO helpingpaw.contactinformation VALUES ("birdsandthings@gmail.com","1800 Murphy Ave","2098001456");
 INSERT INTO helpingpaw.contactinformation VALUES ("djramos@gmail.com","2424 Linden St","4081354187");
 INSERT INTO helpingpaw.contactinformation VALUES ("jason.arg@gmail.com","34 Nob Hill Pl","4081457639");
+INSERT INTO helpingpaw.contactinformation VALUES ("branden.lopez@sjsu.edu","38 Nob Hill Pl","4081457639");
+INSERT INTO helpingpaw.contactinformation VALUES ("joe.joe@sjsu.edu","39 Nob Hill Pl","4081457639");
+INSERT INTO helpingpaw.contactinformation VALUES ("kit.kit@sjsu.edu","39 Nob Hill Pl","1081457639");
+INSERT INTO helpingpaw.contactinformation VALUES ("rabits@sjsu.edu","94 Barton Road","9511457639");
+INSERT INTO helpingpaw.contactinformation VALUES ("lizards@sjsu.edu","Azusa Road","6261457639");
+INSERT INTO helpingpaw.contactinformation VALUES( "mgeller1989@sjsu.edu" , "Pacific Ave","6263539987");
+INSERT INTO helpingpaw.contactinformation VALUES( "mike.wu@sjsu.edu", "None of ur damn business", "1234567890");
+INSERT INTO helpingpaw.contactinformation VALUES( "monica.lewis@sjsu.edu", "Sabado Tarde", "8053789456");
+INSERT INTO helpingpaw.contactinformation VALUES("liz.hill@sjsu.edu", "Trigo Road","805674123");
+INSERT INTO helpingpaw.contactinformation VALUES("jess.ramos@sjsu.edu", "El Pescino", "8055086547");
+INSERT INTO helpingpaw.contactinformation VALUES("mary.katy@sjsu.edu" ,"Domingo", "8056269091");
 
-CREATE TABLE helpingpaw.contains (email VARCHAR(45) PRIMARY KEY, username VARCHAR(45) PRIMARY KEY);
+CREATE TABLE helpingpaw.contains (email VARCHAR(45) PRIMARY KEY, username VARCHAR(45));
 INSERT INTO helpingpaw.contains VALUES ("mj300@gmail.com","mj300");
 INSERT INTO helpingpaw.contains VALUES ("ronniem@yahoo.com","ronniem");
 INSERT INTO helpingpaw.contains VALUES ("puppyluvr@hotmail.com","puppyluvr");
@@ -139,6 +151,18 @@ INSERT INTO helpingpaw.contains VALUES ("s.coleman@gmail.com","sarahcoleman");
 INSERT INTO helpingpaw.contains VALUES ("birdsandthings@gmail.com","birdsandthings");
 INSERT INTO helpingpaw.contains VALUES ("djramos@gmail.com","djramos");
 INSERT INTO helpingpaw.contains VALUES ("jason.arg@gmail.com","argonaut2000");
+INSERT INTO helpingpaw.contains VALUES ("branden.lopez@sjsu.edu","brandenL");
+INSERT INTO helpingpaw.contains VALUES ("joe.joe@sjsu.edu","hamsterhut");
+INSERT INTO helpingpaw.contains VALUES ("kit.kit@sjsu.edu","kitkatclub");
+INSERT INTO helpingpaw.contains VALUES( "rabits@sjsu.edu" , "rabbitsonrabbits");
+INSERT INTO helpingpaw.contains VALUES( "lizards@sjsu.edu" , "lizardlounge");
+INSERT INTO helpingpaw.contains VALUES( "mgeller1989@sjsu.edu" , "mgeller1989");
+INSERT INTO helpingpaw.contains VALUES( "mike.wu@sjsu.edu" , "toomanydogs");
+INSERT INTO helpingpaw.contains VALUES( "monica.lewis@sjsu.edu" , "monicalewis");
+INSERT INTO helpingpaw.contains VALUES("liz.hill@sjsu.edu", "lizhill");
+INSERT INTO helpingpaw.contains VALUES("jess.ramos@sjsu.edu" ,"jessramos");
+INSERT INTO helpingpaw.contains VALUES("mary.katy@sjsu.edu" ,"marykate");
+
 
 Create TABLE helpingpaw.Dog(
 animalID integer PRIMARY KEY,
@@ -168,20 +192,20 @@ animalID integer,
 Primary Key(postID, animalID)
 );
 INSERT INTO helpingpaw.InPost values( 1 , 1 );
-INSERT INTO helpingpaw.InPost values( 2 , 1 );
-INSERT INTO helpingpaw.InPost values( 3 , 2 );
-INSERT INTO helpingpaw.InPost values( 4 , 3 );
-INSERT INTO helpingpaw.InPost values( 5 , 4);
-INSERT INTO helpingpaw.InPost values( 6 ,5 );
-INSERT INTO helpingpaw.InPost values( 7 , 6 );
-INSERT INTO helpingpaw.InPost values( 8 , 7);
-INSERT INTO helpingpaw.InPost values( 9 ,  8);
-INSERT INTO helpingpaw.InPost values( 10 ,  9);
-INSERT INTO helpingpaw.InPost values( 11 ,  10);
-INSERT INTO helpingpaw.InPost values( 12 ,  11);
-INSERT INTO helpingpaw.InPost values( 13 , 12);
-INSERT INTO helpingpaw.InPost values( 14 ,  13);
-INSERT INTO helpingpaw.InPost values( 15 ,  14);
+INSERT INTO helpingpaw.InPost values( 2 , 2 );
+INSERT INTO helpingpaw.InPost values( 3 , 3 );
+INSERT INTO helpingpaw.InPost values( 4 , 4 );
+INSERT INTO helpingpaw.InPost values( 5 , 5);
+INSERT INTO helpingpaw.InPost values( 6 , 6 );
+INSERT INTO helpingpaw.InPost values( 7 , 7 );
+INSERT INTO helpingpaw.InPost values( 8 , 8);
+INSERT INTO helpingpaw.InPost values( 9 , 9);
+INSERT INTO helpingpaw.InPost values( 10 , 10);
+INSERT INTO helpingpaw.InPost values( 11 , 11);
+INSERT INTO helpingpaw.InPost values( 12 , 12);
+INSERT INTO helpingpaw.InPost values( 13 , 13);
+INSERT INTO helpingpaw.InPost values( 14 , 14);
+INSERT INTO helpingpaw.InPost values( 15 , 15);
 
 Create TABLE helpingpaw.other(
 animalID integer PRIMARY KEY,
@@ -259,3 +283,59 @@ INSERT INTO helpingpaw.users VALUES("sarahcoleman","user","password123");
 INSERT INTO helpingpaw.users VALUES("birdsandthings","agency","BirdsForever");
 INSERT INTO helpingpaw.users VALUES("djramos","user","discjockey456");
 INSERT INTO helpingpaw.users VALUES("argonaut2000","user","piratelifeforme");
+
+# Links sellers and posts 
+CREATE TABLE helpingpaw.adds(postID integer , sellerID integer );
+INSERT INTO helpingpaw.adds values( 1 , 1 );
+INSERT INTO helpingpaw.adds values( 2 , 2 );
+INSERT INTO helpingpaw.adds values( 3 , 3 );
+INSERT INTO helpingpaw.adds values( 4 , 4 );
+INSERT INTO helpingpaw.adds values( 5 , 5 );
+INSERT INTO helpingpaw.adds values( 6 , 6 );
+INSERT INTO helpingpaw.adds values( 7 , 7 );
+INSERT INTO helpingpaw.adds values( 8 , 8 );
+INSERT INTO helpingpaw.adds values( 9 , 9 );
+INSERT INTO helpingpaw.adds values( 10 , 10 );
+INSERT INTO helpingpaw.adds values( 11 , 11 );
+INSERT INTO helpingpaw.adds values( 12 , 12 );
+INSERT INTO helpingpaw.adds values( 13 , 13 );
+INSERT INTO helpingpaw.adds values( 14 , 14 );
+INSERT INTO helpingpaw.adds values( 15 , 15 );
+
+
+# Links adopter and posts 
+CREATE TABLE helpingpaw.removes(postID integer , adopterID integer );
+INSERT INTO helpingpaw.removes values( 1 , 15 );
+INSERT INTO helpingpaw.removes values( 2 , 12 );
+INSERT INTO helpingpaw.removes values( 3 , 13 );
+INSERT INTO helpingpaw.removes values( 4 , 14 );
+INSERT INTO helpingpaw.removes values( 5 , 15 );
+INSERT INTO helpingpaw.removes values( 6 , 16 );
+INSERT INTO helpingpaw.removes values( 7 , 17 );
+INSERT INTO helpingpaw.removes values( 8 , 18 );
+INSERT INTO helpingpaw.removes values( 9 , 19 );
+INSERT INTO helpingpaw.removes values( 10 , 1 );
+INSERT INTO helpingpaw.removes values( 11 , 1 );
+INSERT INTO helpingpaw.removes values( 12 , 2 );
+INSERT INTO helpingpaw.removes values( 13 , 3 );
+INSERT INTO helpingpaw.removes values( 14 , 4 );
+INSERT INTO helpingpaw.removes values( 15 , 5 );
+
+
+# Links Admin to post  
+CREATE TABLE helpingpaw.updates(postID integer , adopterID integer );
+INSERT INTO helpingpaw.updates values( 1 , 15 );
+INSERT INTO helpingpaw.updates values( 2 , 12 );
+INSERT INTO helpingpaw.updates values( 3 , 13 );
+INSERT INTO helpingpaw.updates values( 4 , 14 );
+INSERT INTO helpingpaw.updates values( 5 , 15 );
+INSERT INTO helpingpaw.updates values( 6 , 16 );
+INSERT INTO helpingpaw.updates values( 7 , 17 );
+INSERT INTO helpingpaw.updates values( 8 , 18 );
+INSERT INTO helpingpaw.updates values( 9 , 19 );
+INSERT INTO helpingpaw.updates values( 10 , 1 );
+INSERT INTO helpingpaw.updates values( 11 , 1 );
+INSERT INTO helpingpaw.updates values( 12 , 2 );
+INSERT INTO helpingpaw.updates values( 13 , 3 );
+INSERT INTO helpingpaw.updates values( 14 , 4 );
+INSERT INTO helpingpaw.updates values( 15 , 5 );
